cef2f88d2f5625c6e922789e3c921b8ad05ade94
edwinvalles <edwinvalles@gmail.com>
2020-11-13 16:42:49 +0800

    code along nov 10 session

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,42 @@
 class ApplicationController < ActionController::Base
+
+    def index
+        @articles = Article.all
+    end
+
+    def show
+
+    end
+
+    def new
+        @article = Article.new
+    end
+
+    def create
+        @article = Article.new(article_params)
+        if @article.save
+            redirect_to articles_path
+        else
+            render :new
+        end
+    end 
+
+    def edit
+
+    end 
+
+    def update
+
+    end
+
+    def delete
+
+    end
+
+    private
+    def article_params
+        params.require(:article).permit(:name, :body)
+    end 
+
+
 end
