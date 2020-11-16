2ceff9040aa4931eded2cda124099fe60106a45f
edwinvalles <edwinvalles@gmail.com>
2020-11-16 20:58:14 +0800

    adding delete functions and adjustment sql to dev

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -27,7 +27,6 @@ class ApplicationController < ActionController::Base
 
     def update
         @article = Article.find(params[:id])
-
         if @article.update(article_params)
             redirect_to articles_path
         else
@@ -37,7 +36,6 @@ class ApplicationController < ActionController::Base
 
     def delete
         @article = Article.find(params[:id])
-
         if @article.destroy
             redirect_to articles_path
         else
@@ -49,6 +47,5 @@ class ApplicationController < ActionController::Base
     def article_params
         params.require(:article).permit(:name, :body)
     end 
-
 
 end
